ceb0f44e695caeef225b5dc87bdb8b474c7e5e68
lambdaDove <zhang5918@foxmail.com>
2021-12-22 19:29:03 +0800

    closed-book qa

---
+++ 2018202098/src/scripts/bart.py
@@ -0,0 +1,50 @@
+import torch
+import torch.nn.functional as F
+from torch import Tensor, nn
+from transformers import T5ForConditionalGeneration, BartForConditionalGeneration
+from transformers.modeling_outputs import Seq2SeqLMOutput
+
+
+# This function is copied from modeling_bart.py
+def shift_tokens_right(input_ids, pad_token_id):
+    """Shift input ids one token to the right, and wrap the last non pad token (usually <eos>)."""
+    prev_output_tokens = input_ids.clone()
+    index_of_eos = (input_ids.ne(pad_token_id).sum(dim=1) - 1).unsqueeze(-1)
+    prev_output_tokens[:, 0] = input_ids.gather(1, index_of_eos).squeeze()
+    prev_output_tokens[:, 1:] = input_ids[:, :-1]
+    return prev_output_tokens
+
+class ClosedQABart(BartForConditionalGeneration):
+    def forward(self, input_ids, attention_mask=None, encoder_outputs=None,
+            decoder_input_ids=None, decoder_attention_mask=None,
+            is_training=False, **kwargs):
+
+        if is_training:
+            _decoder_input_ids = shift_tokens_right(decoder_input_ids, self.config.pad_token_id)
+        else:
+            _decoder_input_ids = decoder_input_ids
+
+        outputs = self.model(
+            input_ids,
+            attention_mask=attention_mask,
+            encoder_outputs=encoder_outputs,
+            decoder_input_ids=_decoder_input_ids,
+            decoder_attention_mask=decoder_attention_mask,
+            **kwargs
+        )
+        lm_logits = F.linear(outputs[0], self.model.shared.weight, bias=self.final_logits_bias)
+        if is_training:
+            loss_fct = nn.CrossEntropyLoss(reduction="mean", ignore_index=self.config.pad_token_id)
+            loss = loss_fct(lm_logits.view(-1, self.config.vocab_size),
+                              decoder_input_ids.view(-1))
+            return loss
+        return Seq2SeqLMOutput(
+            logits=lm_logits,
+            decoder_hidden_states=outputs.decoder_hidden_states,
+            decoder_attentions=outputs.decoder_attentions,
+            cross_attentions=outputs.cross_attentions,
+            encoder_last_hidden_state=outputs.encoder_last_hidden_state,
+            encoder_hidden_states=outputs.encoder_hidden_states,
+            encoder_attentions=outputs.encoder_attentions,
+        )
+
